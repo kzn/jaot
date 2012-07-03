96bb90c36d27a7d7d6502e70089f9acac3c9df8d
kazennikov <kazennikov@gmail.com>
2012-07-03 17:57:28 +0400

    creating compiler for FSA

--- src/main/java/name/kazennikov/morph/aot/MorphCompiler.java
+++ src/main/java/name/kazennikov/morph/aot/MorphCompiler.java
@@ -1,96 +1,264 @@
 package name.kazennikov.morph.aot;
 
-import gnu.trove.iterator.TIntIterator;
+import gnu.trove.list.TIntList;
 import gnu.trove.list.array.TIntArrayList;
 import gnu.trove.map.hash.TObjectIntHashMap;
+import gnu.trove.procedure.TObjectIntProcedure;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.swing.event.InternalFrameAdapter;
 import javax.xml.bind.JAXBException;
 
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.Multiset;
+import org.omg.CORBA.FREE_MEM;
 
 import name.kazennikov.dafsa.CharFSA;
 import name.kazennikov.dafsa.CharTrie;
-import name.kazennikov.dafsa.GenericTrie;
 import name.kazennikov.dafsa.GenericFSA;
+import name.kazennikov.dafsa.GenericTrie;
 import name.kazennikov.dafsa.IntFSA;
-import name.kazennikov.dafsa.IntTrie;
+
+import com.google.common.base.Objects;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
 
 public class MorphCompiler {
 	
-	public static class Transition {
-		int state;
-		char label;
-		int next;
-	}
-	
-	MorphLanguage morphLanguage;
-	MorphDict morphDict;
-	public static final Character[] charMap = new Character[65536];
-	
-	static {
-		for(int i = 0; i < charMap.length; i++) {
-			charMap[i] = new Character((char)i);
+	public static class GuesserEnding {
+		TIntList ending;
+		int feats;
+		
+		public GuesserEnding(TIntList ending, int feats) {
+			super();
+			this.ending = ending;
+			this.feats = feats;
 		}
-	}
-	
-	public MorphCompiler(MorphLanguage morphLanguage, MorphDict morphDict) {
-		this.morphDict = morphDict;
-		this.morphLanguage = morphLanguage;
+		
+		@Override 
+		public int hashCode() {
+			return Objects.hashCode(ending, feats);
+		}
+		
+		@Override
+		public boolean equals(Object other) {
+			if(!(other instanceof GuesserEnding))
+				return false;
+			
+			GuesserEnding o = (GuesserEnding) other;
+			
+			return Objects.equal(ending, o.ending) && Objects.equal(feats, o.feats);
+		}
+
+		public TIntList getEnding() {
+			return ending;
+		}
+
+		public int getFeats() {
+			return feats;
+		}
+		
+		
+		
 		
 	}
 	
-    public static List<Character> expand(String s) {
-        List<Character> chars = new ArrayList<Character>();
-
-        for(int i = 0; i != s.length(); i++) {
-            chars.add(charMap[s.charAt(i)]);
-        }
-        return chars;
-    }
-    
-    public static List<Character> expandReverse(String s) {
-        List<Character> chars = new ArrayList<Character>();
-
-        for(int i = 0; i != s.length(); i++) {
-            chars.add(charMap[s.charAt(s.length() - 1 - i)]);
-        }
-        return chars;
-    }
-
-    
-    /*public static List<Pair<Character, Character>> expand(String wordForm, String lemma) {
-        List<Pair<Character, Character>> chars = new ArrayList<Pair<Character, Character>>();
+	MorphLanguage morphLanguage;
+	
+	public MorphCompiler(MorphLanguage morphLanguage) {
+		this.morphLanguage = morphLanguage;	
+	}
+	
+	public void compile(MorphDict md, int predictionDepth, int predictionFreq, File dest) throws IOException {
+		
+		CharFSA fsa = new CharFSA(new CharFSA.SimpleNode());
+		IntFSA fst = new IntFSA(new IntFSA.SimpleNode());
+		
+		IntFSA fstGuesser = new IntFSA(new IntFSA.SimpleNode());
         
-        int len = Math.max(wordForm.length(), lemma.length());
+        TObjectIntHashMap<BitSet> featSets = new TObjectIntHashMap<BitSet>();
 
-        for(int i = 0; i != len; i++) {
-            char wfChar = i < wordForm.length()? wordForm.charAt(i) : 0;
-            char lemmaChar = i < lemma.length()? lemma.charAt(i) : 0;
-            chars.add(Pair.of(charMap[wfChar], charMap[lemmaChar]));
-        }
-        return chars;
-    }*/
+        int wfNum = 0;
+        int lemmaNum = 0;
+        Multiset<String> predictionSet = HashMultiset.create();
+        Multiset<GuesserEnding> fstPredictionSet = HashMultiset.create();
+        TIntArrayList fstSeq = new TIntArrayList(32);
+        // main loop over the dictionary
+        long elapsed = System.currentTimeMillis();
+		for(MorphDict.Lemma lemma : md.getLemmas()) {
+	        lemmaNum++;
+			for(MorphDict.WordForm wf : lemma.expand()) {
+				BitSet feats = morphLanguage.getWordFormFeats(wf.getFeats(), wf.getCommonAnCode());
+				int featId = featSets.get(feats);
+				
+				if(featId == 0) {
+					featId = featSets.size() + 1;
+					featSets.put(feats, featId);
+				}
+				
+				wfNum++;
+				
+				final String wordForm = wf.getWordForm();
+				expand(fstSeq, wf.getWordForm(), wf.getLemma());
+				
+				predictionSet.add(new String(wordForm.substring(wordForm.length() > 5? wordForm.length() - 1 - 5 : 0)));
+				fsa.addMinWord(wordForm, featId);
+				fst.addMinWord(fstSeq, featId);
+				
+				TIntList ending = fstSeq.subList(fstSeq.size() > predictionDepth? fstSeq.size() - predictionDepth - 1: 0, fstSeq.size());
+				ending.reverse();
+				fstPredictionSet.add(new GuesserEnding(ending, featId));
+			}
+		}
+		
+	
+		elapsed = System.currentTimeMillis() - elapsed;
+		System.out.printf("Elapsed on fsa building:%d ms%n", elapsed);
 
-    public static class FSANode extends GenericTrie.SimpleNode<Character, Set<Integer>, Integer> {
+		
+		for(Multiset.Entry<GuesserEnding> e : fstPredictionSet.entrySet()) {
+			if(e.getCount() < predictionFreq)
+				continue;
+			
+			
+			fstGuesser.addMinWord(e.getElement().getEnding(), e.getElement().getFeats());
+		}
 
-        public FSANode(Set<Integer> fin) {
-            super(fin);
-        }
+		elapsed = System.currentTimeMillis();
+		write(dest, featSets, fsa, fst, fstGuesser);
+		elapsed = System.currentTimeMillis() - elapsed;
+		
+		System.out.printf("Elapsed on writing:%d ms%n", elapsed);
+	}
+	
+	private void write(File dest,TObjectIntHashMap<BitSet> featSets, CharFSA fsa,	IntFSA fst, IntFSA fstGuesser) throws IOException {
+		DataOutputStream dos = null;
+		try {
+			dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(dest)));
+			write(dos, featSets);
+			fsa.write(new CharFSAWriter(dos));
+			fst.write(new IntFSAWriter(dos));
+			fstGuesser.write(new IntFSAWriter(dos));
+		} finally {
+			if(dos != null)
+				dos.close();
+		}
+		
+	}
 
-        @Override
-        public GenericTrie.SimpleNode makeNode() {
-            return new FSANode(new HashSet<Integer>());
-        }
-    }
+	public void write(DataOutputStream s, TObjectIntHashMap<BitSet> featSets) throws IOException {
+		final BitSet[] sets = new BitSet[featSets.size() + 1];
+		sets[0] = new BitSet();
+		featSets.forEachEntry(new TObjectIntProcedure<BitSet>() {
+			@Override
+			public boolean execute(BitSet a, int b) {
+				sets[b] = a;
+				return true;
+			}
+		});
+
+		s.writeInt(sets.length);
+
+		for(BitSet featSet : sets) {
+			s.writeInt(featSet.cardinality());
+			for(int i = 0; i != featSet.length(); i++) {
+				if(featSet.get(i)) {
+					s.writeInt(i);
+				}
+			}
+		}
+	}
+	
+	public static class IntFSAWriter implements IntFSA.Events { 
+		DataOutputStream s;
+
+		public IntFSAWriter(DataOutputStream s) {
+			this.s = s;
+		}
+
+		@Override
+		public void states(int states) throws IOException {
+			s.writeInt(states);
+		}
+
+		@Override
+		public void state(int state) throws IOException {
+			s.writeInt(state);
+		}
+
+		@Override
+		public void finals(int n) throws IOException {
+			s.writeInt(n);
+		}
+
+		@Override
+		public void stateFinal(int fin) throws IOException {
+			s.writeInt(fin);
+		}
+
+		@Override
+		public void transitions(int n) throws IOException {
+			s.writeInt(n);
+			
+		}
+
+		@Override
+		public void transition(int input, int dest) throws IOException {
+			s.writeInt(input);
+			s.writeInt(dest);
+		}
+	}
+	
+	public static class CharFSAWriter implements CharFSA.Events { 
+		DataOutputStream s;
+
+		public CharFSAWriter(DataOutputStream s) {
+			this.s = s;
+		}
+
+		@Override
+		public void states(int states) throws IOException {
+			s.writeInt(states);
+		}
+
+		@Override
+		public void state(int state) throws IOException {
+			s.writeInt(state);
+		}
+
+		@Override
+		public void finals(int n) throws IOException {
+			s.writeInt(n);
+		}
+
+		@Override
+		public void stateFinal(int fin) throws IOException {
+			s.writeInt(fin);
+		}
+
+		@Override
+		public void transitions(int n) throws IOException {
+			s.writeInt(n);
+			
+		}
+
+		@Override
+		public void transition(char input, int dest) throws IOException {
+			s.writeInt(input);
+			s.writeInt(dest);
+		}
+	}
+
+	
     
     public static void expand(TIntArrayList dest, String wf, String lemma) {
     	dest.clear();
@@ -113,17 +281,6 @@ public class MorphCompiler {
     	}
     }
     
-    /*public static class FSTNode extends Trie.SimpleNode<Pair<Character, Character>, Set<Integer>, Integer> {
-
-        public FSTNode(Set<Integer> fin) {
-            super(fin);
-        }
-
-        @Override
-        public Trie.SimpleNode makeNode() {
-            return new FSTNode(new HashSet<Integer>());
-        }
-    }*/
 	public static void main(String[] args) throws JAXBException, IOException {
 		MorphConfig mc = MorphConfig.newInstance(new File("russian.xml"));
 		
@@ -132,101 +289,9 @@ public class MorphCompiler {
 		long st = System.currentTimeMillis();
 		MorphDict md = ml.readDict();
 		
+		MorphCompiler morphCompiler = new MorphCompiler(ml);
 		
-		CharTrie chTrie = new CharTrie();
-		CharTrie chTrie1 = new CharTrie();
-		CharFSA chfsa = new CharFSA(new CharFSA.SimpleNode());
-		IntFSA intfsa = new IntFSA(new IntFSA.SimpleNode());
-        GenericFSA<Character, Set<Integer>, Integer> trieFsa =
-                new GenericFSA<Character, Set<Integer>, Integer>(new FSANode(new HashSet<Integer>()));
-        
-        /*SimpleTrie<Pair<Character, Character>, Set<Integer>, Integer> trieFst =
-                new SimpleTrie<Pair<Character, Character>, Set<Integer>, Integer>(new FSTNode(new HashSet<Integer>()));
-        
-        SimpleTrie<Pair<Character, Character>, Set<Integer>, Integer> reverseTrieFst =
-                new SimpleTrie<Pair<Character, Character>, Set<Integer>, Integer>(new FSTNode(new HashSet<Integer>()));*/
-
-
-        
-        TObjectIntHashMap<BitSet> featSets = new TObjectIntHashMap<BitSet>();
-
-        StringBuilder sb = new StringBuilder();
-        TIntArrayList intLabels = new TIntArrayList(32);
-        int wfNum = 0;
-        Multiset<String> predictionSet = HashMultiset.create();
-        
-        
-		for(MorphDict.Lemma lemma : md.lemmas) {
-			for(MorphDict.WordForm wf : lemma.expand()) {
-				BitSet feats = ml.getWordFormFeats(wf.feats, wf.commonAnCode);
-				int featId = featSets.get(feats);
-				
-				if(featId == 0) {
-					featId = featSets.size() + 1;
-					featSets.put(feats, featId);
-				}
-				
-				//sb.setLength(0);
-				//sb.append(wf.wordForm).reverse();
-				wfNum++;
-				
-				if(wfNum % 10000 == 0) {
-					System.out.printf("%d wordforms, fsa size: %d%n", wfNum, chTrie.size());
-				}
-				
-				//trieFsa.addMinWord(expand(wf.wordForm), featId);
-				//chfsa.add(wf.wordForm, featId);
-				final String s = wf.wordForm;
-				predictionSet.add(new String(s.substring(s.length() > 5? s.length() - 1 - 5 : 0)));
-				//chTrie.add(wf.wordForm, featId);
-				//expand(intLabels, wf.wordForm);
-				//expand(intLabels, wf.wordForm, wf.lemma);
-				//chfsa.addMinWord(s, featId);
-				//intfsa.addMinWord(intLabels, featId);
-				
-				/*if(chfsa.size() != intfsa.size()) {
-					wfNum = wfNum;
-				}*/
-				
-
-				
-//				chTrie1.add(new TIntIterator() {
-//					int pos = 0;
-//
-//					@Override
-//					public void remove() {
-//
-//					}
-//
-//					@Override
-//					public boolean hasNext() {
-//						return pos < s.length();
-//					}
-//
-//					@Override
-//					public int next() {
-//						return s.charAt(pos++);
-//					}
-//				}, featId);
-//				
-//				if(chTrie.size() != chTrie1.size()) {
-//					wfNum = wfNum;
-//				}
-				
-                //trieFst.addMinWord(expand(wf.wordForm, wf.lemma), featId);
-				//reverseTrieFst.addMinWord(expand(sb.toString(), new StringBuilder(wf.lemma).reverse().toString()), featId);
-			}
-			
-		}
-
-		st = System.currentTimeMillis() - st;
-		System.out.printf("Elapsed: %d ms%n", st);
-        System.out.printf("FSA size: %d%n", trieFsa.size());
-        System.out.printf("charFSA size: %d%n", chfsa.size());
-        System.out.printf("intFSA size: %d%n", intfsa.size());
-        //System.out.printf("FST size: %d%n", reverseTrieFst.size());
-        //System.out.printf("Reverse FST size: %d%n", trieFst.size());
-		System.out.printf("Dict size: %d%n", md.lemmas.size());
+		morphCompiler.compile(md, 5, 0, new File("russian.dat"));
 		
 	}
 	
