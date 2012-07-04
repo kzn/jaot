1c2749785077217435e1d85dbd73f37103446e47
kazennikov <kazennikov@gmail.com>
2012-07-04 17:10:48 +0400

    print parses

--- src/main/java/name/kazennikov/morph/aot/MorphData.java
+++ src/main/java/name/kazennikov/morph/aot/MorphData.java
@@ -1,5 +1,8 @@
 package name.kazennikov.morph.aot;
 
+import gnu.trove.procedure.TIntProcedure;
+import gnu.trove.set.TIntSet;
+
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.File;
@@ -8,6 +11,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
+
+import javax.xml.bind.JAXBException;
 
 import name.kazennikov.dafsa.IntNFSA;
 import name.kazennikov.dafsa.IntNFSA.IntNFSABuilder;
@@ -63,10 +68,10 @@ public class MorphData {
 			IntNFSABuilder intFSTBuilder = new IntNFSABuilder();
 			IntTrie.Reader.read(s, intFSTBuilder, 4);
 			IntNFSA fst = intFSTBuilder.build();
-			IntTrie.Reader.read(s, intFSTBuilder, 4);
-			IntNFSA guesser = intFSTBuilder.build();
+			//IntTrie.Reader.read(s, intFSTBuilder, 4);
+			//IntNFSA guesser = intFSTBuilder.build();
 			
-			return new MorphData(featSets, fsa, fst, guesser);
+			return new MorphData(featSets, fsa, fst, null);
 			
 			
 		} finally {
@@ -76,9 +81,81 @@ public class MorphData {
 		
 	}
 	
-	public static void main(String[] args) throws IOException {
-		MorphData mdata = MorphData.read(new File("russian.dat"));
+	
+	public static interface ParseProcessor {
+		public boolean process(CharSequence s, StringBuilder out, int startIndex, int endIndex, TIntSet fin);
 	}
+	
+	protected void walkIterativeInternal(IntNFSA fst, CharSequence s, StringBuilder sb, int startIndex, int endIndex, int currentIndex, 
+			int state, char ch, ParseProcessor parseProcessor) {
+		long value = fst.getTransitionsInfo(state, ch);
+		int start = fst.getTransitionsStart(value);
+		int end = start + fst.getTransitionsLength(value);
+		
+		while(start < end) {
+			
+			char outCh = (char) fst.getTransitionOut(start);
+			int nextState = fst.getTransitionNext(start);
+			if(outCh != 0)
+				sb.append(outCh);
+			walkIterative(fst, s, sb, startIndex, endIndex, currentIndex != endIndex? currentIndex + 1: endIndex, 
+					nextState, parseProcessor);
+			if(outCh != 0)
+				sb.deleteCharAt(sb.length() - 1);
+
+			start++;
+		}
+
+	}
+	
+	public void walkIterative(IntNFSA fst, CharSequence s, StringBuilder sb, int startIndex, int endIndex, int currentIndex, 
+			int state, ParseProcessor parseProcessor) {
+		TIntSet fin = fst.getFinals(state);
+
+			if(fin != null && !fin.isEmpty()) {
+				parseProcessor.process(s, sb, startIndex, currentIndex, fin);
+			}
+			
+			char ch = currentIndex < endIndex? s.charAt(currentIndex) : 0;
+			walkIterativeInternal(fst, s, sb, startIndex, endIndex, currentIndex, state, ch, parseProcessor);
+			char toUpper = Character.toUpperCase(ch);
+			if(toUpper != ch) {
+				walkIterativeInternal(fst, s, sb, startIndex, endIndex, currentIndex, state, toUpper, parseProcessor);
+			}
+			
+			// for single-pass morphan on fst only
+			ch = 0;
+			walkIterativeInternal(fst, s, sb, startIndex, endIndex, currentIndex, state, (char)0, parseProcessor);
+
+	}
+	
+	public static void main(String[] args) throws IOException, JAXBException {
+		MorphConfig mc = MorphConfig.newInstance(new File("russian.xml"));
+		
+		final MorphLanguage ml = new MorphLanguage(mc);
+
+		final MorphData mdata = MorphData.read(new File("russian.dat"));
+		
+		mdata.walkIterative(mdata.fst, "МАМЫ", new StringBuilder(), 0, 4, 0, 1, new ParseProcessor() {
+			
+			@Override
+			public boolean process(final CharSequence s, final StringBuilder out, final int startIndex,
+					final int endIndex, TIntSet fin) {
+				fin.forEach(new TIntProcedure() {
+					
+					@Override
+					public boolean execute(int value) {
+						System.out.printf("parse: %s-%s%s%n", s.subSequence(startIndex, endIndex), out, ml.convert(mdata.featSets.get(value)));
+						return true;
+					}
+				});
+
+				return true;
+			}
+		});
+	}
+
+	
 	
 	
 	
